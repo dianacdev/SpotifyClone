c2dbacaecc69be5fbcb9beabd5fa2a60c80eb1b1
Diana C <diana@gmetrix.com>
2023-07-07 00:34:53 -0600

    Liked Songs Implemented!

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,7 +1,38 @@
-const SearchContent = () => {
+"use client"
+
+import LikeButton from '@/components/LikeButton';
+import MediaItem from '@/components/MediaItem';
+import {Song} from '@/types';
+
+interface SearchContentProps{
+    songs: Song[];
+}
+
+const SearchContent: React.FC<SearchContentProps> = ({songs}) => {
+
+    if(songs.length === 0)
+    return (
+        <div className='flex flex-col gap-y-2 w-full px-6 text-neutral-400'>
+            No songs found.
+        </div>
+    )
+
     return ( 
-        <div>
-            Search Content!!!
+        <div className='flex flex-col gap-y-2 w-full px-6'>
+            {songs.map((song)=>(
+                <div
+                key={song.id}
+                className='flex items-center gap-x-4 w-full group'
+                >
+                    <div className='flex-1'>
+                        <MediaItem
+                            data={song}
+                            onClick={()=>{}}
+                        />
+                    </div>
+                    <LikeButton songId={song.id}/>
+                </div>
+            ))}
         </div>
     );
 }
--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -26,6 +26,7 @@ const MediaItem: React.FC<MediaItemProps> = ({data, onClick}) => {
                 flex items-center
                 gap-x-3 cursor-pointer
                 hover:bg-neutral-800/50
+                group-hover:bg-neutral-800/50
                 w-full p-2
                 rounded-md
             "
