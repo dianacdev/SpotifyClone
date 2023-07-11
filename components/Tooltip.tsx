4c1a6c3a9f4edf822560bfaf033eddc228773525
Diana C <diana@gmetrix.com>
2023-07-10 20:12:13 -0600

    updated read me!

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -2,11 +2,25 @@
 
 import * as RadixTooltip from '@radix-ui/react-tooltip'
 
-const Tooltip = () => {
+interface TooltipProps{
+    content: string;
+    children: React.ReactNode;
+}
+
+const Tooltip: React.FC<TooltipProps> = ({content, children}) => {
     return ( 
-        <div>
-            Tooltip
-        </div>
+        <RadixTooltip.Provider delayDuration={800} skipDelayDuration={500}>
+        <RadixTooltip.Root>
+          <RadixTooltip.Trigger asChild>
+            {children}
+          </RadixTooltip.Trigger>
+          <RadixTooltip.Portal>
+            <RadixTooltip.Content className="rounded-md py-[10px] px-[15px] " sideOffset={5}>
+            {content}
+            </RadixTooltip.Content>
+          </RadixTooltip.Portal>
+        </RadixTooltip.Root>
+      </RadixTooltip.Provider>
      );
 }
  
